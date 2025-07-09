91ddd5078118f7e21bb33614bb751e28279789f3
David Rajchenberg <davidhk28@gmail.com>
2025-07-09 13:57:41 +0300

    Initial commit - Taboola Android Demo

---
+++ app/src/main/java/com/taboola/taboolademo/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.taboola.taboolademo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.taboola.taboolademo.ui.theme.TaboolaDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TaboolaDemoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    ArticleList(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ArticleList(modifier: Modifier = Modifier) {
+    val articles = List(10) {"Article #${it + 1}"}
+
+    LazyColumn(modifier = modifier) {
+        items(articles) {article ->
+            Card(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = article, modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ArticleListPreview() {
+    TaboolaDemoTheme {
+        ArticleList()
+    }
+}
